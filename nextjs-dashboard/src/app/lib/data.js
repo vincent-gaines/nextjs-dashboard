d79e5e102d1ad4e008ab86666676290885ed0c2f
vincent-gaines <vincentanggaines@gmail.com>
2025-06-03 21:43:46 -0400

    no store

--- nextjs-dashboard/src/app/lib/data.js
+++ nextjs-dashboard/src/app/lib/data.js
@@ -1,9 +1,11 @@
 import { sql } from '@vercel/postgres';
 import { formatCurrency } from './utils';
+import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
   // Add noStore() here to prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
+  noStore();
 
   try {
     // Artificially delay a response for demo purposes.
@@ -24,6 +26,7 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
+  noStore();
   try {
     const data = await sql`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -44,6 +47,7 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
+  noStore();
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -83,6 +87,7 @@ export async function fetchFilteredInvoices(
   query,
   currentPage,
 ) {
+  noStore();
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -115,6 +120,7 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query) {
+  noStore();
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -136,6 +142,7 @@ export async function fetchInvoicesPages(query) {
 }
 
 export async function fetchInvoiceById(id) {
+  noStore();
   try {
     const data = await sql`
       SELECT
@@ -161,6 +168,7 @@ export async function fetchInvoiceById(id) {
 }
 
 export async function fetchCustomers() {
+  
   try {
     const data = await sql`
       SELECT
@@ -179,6 +187,7 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query) {
+  noStore();
   try {
     const data = await sql`
 		SELECT
